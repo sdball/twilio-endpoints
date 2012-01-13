43218a13d251a494f390ec882dc81ac308e419dc
Stephen Ball <sdball@gmail.com>
2012-01-13 11:24:06 -0500

    rails new and rvmrc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwilioEndpoints::Application.config.secret_token = '2de7ba4757b76f9d9b27551695006799817083f52ed7c268b3b1446abe6070c4a2508b990df910eb3c9e486fec58963a984af5e340646ff67ec588d124def727'
